51941318235543e583ad91a668259b90111fd64f
Ivanovswork <destira@mail.ru>
2024-07-02 17:00:59 +0300

    Update model Choice: add 'related_name' on field question

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,8 +5,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     date_pub = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices")
     choice_text = models.CharField(max_length=200)
     voice = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
