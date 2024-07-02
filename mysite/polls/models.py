56470f9866fa1ef55db4a11edd5826695a666842
Ivanovswork <destira@mail.ru>
2024-07-02 16:38:29 +0300

    Add models: Question, Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    voice = models.IntegerField(default=0)
