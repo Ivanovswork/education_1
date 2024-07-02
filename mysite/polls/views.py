0e0923a21fd091961bd7423511bb1b0b8a3d8cdd
Ivanovswork <destira@mail.ru>
2024-07-02 20:09:34 +0300

    Add pattern of views: detail, result, vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello")
+
+
+def detail(request, question_id):
+    return HttpResponse(question_id)
+
+
+def result(request, question_id):
+    return HttpResponse(question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(question_id)
